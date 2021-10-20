ede531b0d3e6b4ce36d551dc578fd450c34d75aa
Adrian Puscasu <apu@f5it.com>
2021-10-21 00:11:07 +0300

    adds user registration

--- ADO.NET_Demo.Web/Controllers/ValuesController.cs
+++ ADO.NET_Demo.Web/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ADO.NET_Demo.Web.Areas.Identity.Data;
 using ADO.NET_Demo.Web.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -15,14 +17,17 @@ namespace ADO.NET_Demo.Web.Controllers
     {
         private readonly ILoginService _loginService;
         private readonly ILogger<ValuesController> _logger;
+        private readonly UserManager<AppUser> _userManager;
 
         public ValuesController(ILoginService loginService,
-            ILogger<ValuesController> logger
+            ILogger<ValuesController> logger,
+            UserManager<AppUser> userManager
 
             )
         {
             _loginService = loginService ?? throw new ArgumentNullException(nameof(loginService));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
         }
 
         [HttpGet("getFruits")]
@@ -74,9 +79,25 @@ namespace ADO.NET_Demo.Web.Controllers
 
         [AllowAnonymous]
         [HttpPost("register")]
-        public async Task<ActionResult> SignIn(SignInDto signInDto)
+        public async Task<ActionResult> Register(SignInDto signInDto)
         {
-            return Ok();
+            if (!string.Equals(signInDto.Password, signInDto.ConfirmPassword, StringComparison.Ordinal))
+            {
+                throw new Exception("Password differs from ConfirmPassword");
+            }
+            var appUser = new AppUser
+            {
+                Email = signInDto.Email,
+                UserName = signInDto.Email,
+                EmailConfirmed = false
+            };
+
+            var result = await _userManager.CreateAsync(appUser, signInDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok(new { Result= "Register success" });
+            }
+            return BadRequest(new { Errors = result.Errors });
         }
     }
 }
